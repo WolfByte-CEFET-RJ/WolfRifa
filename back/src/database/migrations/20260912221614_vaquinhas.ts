4ec93218b931505b6b055a03f624ff4c651c1230
Thiago Souza Da Silva <Thiago.souza.th3@gmail.com>
2026-09-18 15:30:47 -0300

    feat: cria a tabela knex da campanha, vaquinha e rifa

--- back/src/database/migrations/20260912221614_vaquinhas.ts
+++ back/src/database/migrations/20260912221614_vaquinhas.ts
@@ -1,10 +1,21 @@
-import type { Knex } from "knex";
 
+
+import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-}
+  return knex.schema.createTable("crowdfundings", (table) => {
+    table.increments("id").primary();
+    
+    
+    table.decimal("raised_amount", 10, 2).defaultTo(0.00);
 
+    table.integer("campaign_id").unsigned().notNullable().references("id").inTable("campaigns").onDelete("CASCADE");
+
+  });
+}
 
 export async function down(knex: Knex): Promise<void> {
-}
 
+  return knex.schema.dropTableIfExists("crowdfundings");
+  
+}
