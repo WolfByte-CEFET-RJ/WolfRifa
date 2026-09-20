2414811941dd8fd85467d29fc80b5e297c37b6c2
Rafael do Amaral Costa <104049906+jake7038@users.noreply.github.com>
2026-09-20 18:06:11 -0300

    Change public_id column type to UUID

--- back/src/database/migrations/20260912221841_inscricao.ts
+++ back/src/database/migrations/20260912221841_inscricao.ts
@@ -4,7 +4,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('registrations', (table: Knex.CreateTableBuilder) => {
         table.increments('id').primary();
-        table.string('public_id', 36).notNullable().unique();
+        table.uuid('public_id').notNullable().unique();
         table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
         table.integer('campaign_id').notNullable().references('id').inTable('campaigns').onDelete('CASCADE');
         table.boolean('notification_sent').defaultTo(false);
